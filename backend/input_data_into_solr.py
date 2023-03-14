c1b26689d69571308bb305f9705186b82752b775
BenChun <67034610+BenjaminChun@users.noreply.github.com>
2023-03-14 16:08:29 +0800

    feat: functional backend with tech_products data

--- backend/input_data_into_solr.py
+++ backend/input_data_into_solr.py
@@ -1,7 +1,7 @@
 import pysolr
 
 # Create a client instance. The timeout and authentication options are not required.
-solr = pysolr.Solr('http://localhost:8984/solr/', always_commit=True, timeout=10)
+solr = pysolr.Solr('http://localhost:8983/solr/tech_products', always_commit=True, timeout=10)
 
 # Note that auto_commit defaults to False for performance. You can set
 # `auto_commit=True` to have commands always update the index immediately, make
@@ -11,30 +11,9 @@ solr = pysolr.Solr('http://localhost:8984/solr/', always_commit=True, timeout=10
 # Do a health check.
 solr.ping()
 
-# How you'd index data.
-solr.add([
-    {
-        "id": "doc_1",
-        "title": "A test document",
-    },
-    {
-        "id": "doc_2",
-        "title": "The Banana: Tasty or Dangerous?",
-        "_doc": [
-            { "id": "child_doc_1", "title": "peel" },
-            { "id": "child_doc_2", "title": "seed" },
-        ]
-    },
-])
-
-# You can index a parent/child document relationship by
-# associating a list of child documents with the special key '_doc'. This
-# is helpful for queries that join together conditions on children and parent
-# documents.
-
 # Later, searching is easy. In the simple case, just a plain Lucene-style
 # query is fine.
-results = solr.search('bananas')
+results = solr.search('samsung')
 
 # The ``Results`` object stores total results found, by default the top
 # ten most relevant results and any additional data like
@@ -43,24 +22,13 @@ print("Saw {0} result(s).".format(len(results)))
 
 # Just loop over it to access the results.
 for result in results:
-    print("The title is '{0}'.".format(result['title']))
+    print("The name is '{0}'.".format(result['name']))
 
-# For a more advanced query, say involving highlighting, you can pass
-# additional options to Solr.
-results = solr.search('bananas', **{
-    'hl': 'true',
-    'hl.fragsize': 10,
-})
+# # Finally, you can delete either individual documents,
+# solr.delete(id='doc_1')
 
-# You can also perform More Like This searches, if your Solr is configured
-# correctly.
-similar = solr.more_like_this(q='id:doc_2', mltfl='text')
+# # also in batches...
+# solr.delete(id=['doc_1', 'doc_2'])
 
-# Finally, you can delete either individual documents,
-solr.delete(id='doc_1')
-
-# also in batches...
-solr.delete(id=['doc_1', 'doc_2'])
-
-# ...or all documents.
-solr.delete(q='*:*')
+# # ...or all documents.
+# solr.delete(q='*:*')
