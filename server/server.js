54f64d7e0b97bfe2351ae5cdd62971bc618c6c2a
Chew Poshi <chewposhi@gmail.com>
2023-03-17 01:30:28 +0800

    installed solr-node in server dir

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const SolrNode = require('solr-node');
 const app = express();
 
 app.use(express.json());
---
+++ server/test.js
@@ -0,0 +1,41 @@
+const SolrNode = require('solr-node');
+
+var client = new SolrNode({
+    host: '127.0.0.1',
+    port: '8983',
+    core: 'films',
+    protocol: 'http'
+  });
+
+
+const nameQuery = {
+    movie_name:"The Gangster, the Cop, the Devil"
+};
+
+
+
+const searchQuery = client.query()
+.q(nameQuery)
+.qop("OR")
+.addParams({
+        wt: 'json',
+        indent: true
+    })
+ .start(1)
+ .rows(5)
+
+client.search(searchQuery, function (err, result) {
+    if (err) {
+        console.log(err);
+        return;
+    };
+
+    const response = result.response;
+    //console.log(response);
+
+    if (response && response.docs) {
+        response.docs.forEach((doc) => {
+          console.log(doc);
+        })
+      }
+});
