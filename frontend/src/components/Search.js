0f270e0605a849cc122b7cc8ce3bdba06834759d
Chew Poshi <chewposhi@gmail.com>
2023-03-24 19:31:23 +0800

    cleaned up Search.js

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -5,49 +5,17 @@
 import React, { useEffect, useState } from 'react';
 import Scroll from './Scroll';
 import SearchList from './SearchList';
-import { genres } from '../utils/genres';
 
 function Search({movies}) {
 
   const [searchInput, setSearchInput] = useState("");
   const [searchResult, setSearchResult] = useState([{}]);
-  const [genreFilter, setGenreFilter] = useState([]);
-  const [checkedState, setCheckedState] = useState(
-    new Array(genres.length).fill(false)
-  );
-
-  const genreFiltered = movies.filter(
-    movie => {
-      let intersect = genreFilter.filter(x => movie.movie_tags[0].includes(x));
-      return (
-        intersect.length === genreFilter.length || genreFilter.length === 0
-      );
-    }
-  );
-  //console.log(genreFiltered);
 
   // use effect for initial page mount
   useEffect(() => {
     setSearchResult(movies);
   }, []);
 
-  // use effect for genre filter
-  useEffect(() => {
-    setGenreFilter([]);
-    let filter =[];
-    checkedState.map( (checked, index)=> {
-      if(checked){
-        filter.push(genres[index].genre);
-      }
-    }
-    )
-    setGenreFilter(filter);
-  }, [checkedState]);
-
-  // use effect for genre filter
-  useEffect(() => {
-    setSearchResult(genreFiltered);
-  }, [genreFilter]);
 
   const handleChange = e => {
     e.preventDefault();
@@ -66,26 +34,6 @@ function Search({movies}) {
       }
     )
   };
-
-  const handleFilterClick = e => {
-    e.preventDefault();
-    setGenreFilter([]);
-    let filter =[];
-    checkedState.map( (checked, index)=> {
-      if(checked){
-        filter.push(genres[index].genre);
-      }
-    }
-    )
-    setGenreFilter(filter);
-  };
-
-  const handleBoxChecked = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
-    setCheckedState(updatedCheckedState);
-  }
 
   function searchList() {
     return (
@@ -109,27 +57,6 @@ function Search({movies}) {
           onChange = {handleChange}
         />
         <button style={{cursor:'pointer'}} onClick={handleClick}>Search</button>
-
-        <div style={{paddingTop:'20px', display:'flex', justifyContent:'center'}}>
-          {genres.map(({ genre }, index) => {
-          return (
-                  <div style={{display:'flex', flexDirection:'row', padding:'5px'}}>
-                    <p>{genre}</p>
-                    <input
-                      type="checkbox"
-                      id={`custom-checkbox-${index}`}
-                      name={genre}
-                      value={genre}
-                      checked={checkedState[index]}
-                      onChange={() => handleBoxChecked(index)}
-                    />
-                  </div>
-                  
-          );
-        })}
-        </div>
-        <button style={{cursor:'pointer'}} onClick={handleFilterClick}>filter</button>
-        
       </div>
       {searchList()}
     </section>
