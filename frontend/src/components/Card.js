8ddafa77f20687f960f26515e494e26091052dd7
Chew Poshi <chewposhi@gmail.com>
2023-03-25 19:39:19 +0800

    fixed link does not open a new tab problem

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 function Card({movie, isMore}) {
   return(
-    <Link to={`/movie/${movie.id}`}>
+    <Link to={`/movie/${movie.id}` } target="_blank">
       <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5" style={{borderStyle:isMore? 'solid' : 'none'}}>
         <img className="br-100 h3 w3 dib" alt="poster" src={movie.poster_url} />
         <div>
