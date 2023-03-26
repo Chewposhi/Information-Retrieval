da3faec4150c89872b87dd902c661e0df0dbfe12
Chew Poshi <chewposhi@gmail.com>
2023-03-26 19:08:30 +0800

    filtered out ':'

--- server/server.js
+++ server/server.js
@@ -69,8 +69,9 @@ app.get("/movie/:id", (req, res) => {
 });
 
 app.get("/nameSearch/:q", (req, res) => {
+    const filtered = req.params.q.replace(':','');
     const Query = {
-        "movie_name":'"'+req.params.q+'"'
+        "movie_name":'"'+filtered+'"'
     };
 
     const searchQuery = client.query()
@@ -121,7 +122,8 @@ app.get("/MoreLikeThis/:id", (req, res) => {
 
 app.get("/Fuzzy/:searchText", (req, res) => {
     console.log(req.params.searchText);
-    var inputArray = req.params.searchText.split(' ');
+    var inputArray = req.params.searchText.replace(':','');
+    inputArray = inputArray.split(' ');
     for (let i = 0; i < inputArray.length; i++) {
         inputArray[i]="movie_name:"+inputArray[i]+"~3";
     };
