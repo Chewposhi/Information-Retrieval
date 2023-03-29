76a409de6b548d393da14f75e2594c06404df026
Chew Poshi <chewposhi@gmail.com>
2023-03-29 14:40:25 +0800

    updated card image url implementation

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -7,7 +7,7 @@ function Card({movie, isMore}) {
   return(
     <Link to={`/movie/${movie.id}` } target="_blank">
       <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5" style={{borderStyle:isMore? 'solid' : 'none'}}>
-        <img className="br-100 h3 w3 dib" alt="poster" src={movie.poster_url} />
+        <img className="br-50 h8 w4 dib" alt="poster" src={movie.movie_Poster} />
         <div>
           <h1>{movie.movie_name}</h1>
           {!isMore && <h2>genre: {movie.movie_tags}</h2>}
