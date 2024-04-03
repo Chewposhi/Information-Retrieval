99e4772a60efbd59e018586ae66a6013b095e591
Chew Poshi <chewposhi@gmail.com>
2024-04-03 15:29:30 +0800

    added tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MovieDetails from './components/MovieDetails';
 import Search from './components/Search';
+import './index.css';
 
 
 function App() {
