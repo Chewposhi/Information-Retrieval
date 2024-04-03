91457b6242ad3b50c545deebd5bf78fd55d7082a
Chew Poshi <chewposhi@gmail.com>
2024-04-03 20:55:42 +0800

    fixed carousel id bug

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { carouselData } from '../constants/constants';
 
 const Carousel = () => {
@@ -27,43 +28,45 @@ const Carousel = () => {
 
   return (
     <div className="relative mt-6">
-      <h2 className="text-2xl font-bold mb-4">What's hot</h2>
-      <div className="carousel relative flex justify-center items-center">
-        <div className="carousel-container flex gap-6 justify-center items-center">
-          <button
-            className="prev-button bg-gray-800 text-white rounded-full w-10 h-10 flex justify-center items-center focus:outline-none"
-            onClick={handlePrevClick}
-          >
-            &lt;
-          </button>
-          {carouselData.map((item, index) => (
-            <div key={index} className={`relative ${index === currentImageIndex ? 'opacity-100' : 'opacity-0 hidden'}`}>
-              <img
-                src={item.image}
-                alt={item.title}
-                className="carousel-image"
-              />
-              <p className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white px-2 py-1">{item.title}</p>
-            </div>
-          ))}
-          <button
-            className="next-button bg-gray-800 text-white rounded-full w-10 h-10 flex justify-center items-center focus:outline-none"
-            onClick={handleNextClick}
-          >
-            &gt;
-          </button>
-        </div>
-        <div className="absolute bottom-0 left-0 right-0 flex justify-center mb-4">
-          {carouselData.map((_, index) => (
+        <h2 className="text-2xl font-bold mb-4">What's hot</h2>
+        <div className="carousel relative flex justify-center items-center">
+            <div className="carousel-container flex gap-6 justify-center items-center">
             <button
-              key={index}
-              className={`w-4 h-4 rounded-full mx-1 focus:outline-none ${index === currentImageIndex ? 'bg-white' : 'bg-gray-500'}`}
-              onClick={() => handleDotClick(index)}
-            />
-          ))}
+                className="prev-button bg-gray-800 text-white rounded-full w-10 h-10 flex justify-center items-center focus:outline-none"
+                onClick={handlePrevClick}
+            >
+                &lt;
+            </button>
+            {carouselData.map((item, index) => (
+                <Link to={`/movie/${carouselData[currentImageIndex].id}` } target="_blank">
+                    <div key={index} className={`relative ${index === currentImageIndex ? 'opacity-100' : 'opacity-0 hidden'}`}>
+                    <img
+                        src={item.image}
+                        alt={item.title}
+                        className="carousel-image"
+                    />
+                    <p className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white px-2 py-1">{item.title}</p>
+                    </div>
+                </Link>
+            ))}
+            <button
+                className="next-button bg-gray-800 text-white rounded-full w-10 h-10 flex justify-center items-center focus:outline-none"
+                onClick={handleNextClick}
+            >
+                &gt;
+            </button>
+            </div>
+            <div className="absolute bottom-0 left-0 right-0 flex justify-center mb-4">
+            {carouselData.map((_, index) => (
+                <button
+                key={index}
+                className={`w-4 h-4 rounded-full mx-1 focus:outline-none ${index === currentImageIndex ? 'bg-white' : 'bg-gray-500'}`}
+                onClick={() => handleDotClick(index)}
+                />
+            ))}
+            </div>
         </div>
-      </div>
-    </div>
+        </div>
   );
 };
 
--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -2,16 +2,16 @@ export const carouselData = [
     {
         "title":"John Wick Chapter 4",
         "image":"/image/carousel/carousel-john-wick-4.jpg",
-        "id":"tt10366206"
+        "id":"fe90740b-cd6a-4611-9da7-c6d4a649a350"
     },
     {
         "title":"The Last of Us",
         "image":"/image/carousel/carousel-last-of-us-730x400.png",
-        "id":"tt3581920"
+        "id":"c4938af3-d785-4dfa-ae3f-74895f644cb2"
     },
     {
         "title":"The Mandalorian",
         "image":"/image/carousel/carousel-mandalorian.jpg",
-        "id":"tt8111088"
+        "id":"84101f1e-cdea-44a3-92f0-85b4bd39ca8b"
     },
 ]
