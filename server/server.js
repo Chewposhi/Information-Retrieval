9aed3dc530c73718e18f347038af9e35a34ac5ce
Chew Poshi <chewposhi@gmail.com>
2024-04-07 16:34:18 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.get("/movie-rec/:genre/:count?", (req, res) => {
             indent: true
         })
     .start(0)
-    .rows(60)
+    .rows(req.params.count ? req.params.count:60)
 
     client.search(searchQuery, function (err, result) {
         if (err) {
